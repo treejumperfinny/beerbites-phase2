cc93d3cbf27bfffa6007e9d7d50471fd2b6f6ff3
brian corker <bcorker123@gmail.com>
2022-06-02 09:20:22 -0400

    tiny cleanup on CocktailCorner component

--- src/CocktailCorner.js
+++ src/CocktailCorner.js
@@ -1,38 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 function CocktailCorner() {
-  const [drinkPic, setDrinkPic] = useState([])
-  const [drinkName, setDrinkName] = useState([])
+  const [drinkPic, setDrinkPic] = useState([]);
+  const [drinkName, setDrinkName] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.punkapi.com/v2/beers/random')
+    fetch("https://api.punkapi.com/v2/beers/random")
       .then((response) => response.json())
       .then((randoBeer) => {
-        console.log(randoBeer)
-      })
-  })
+        console.log(randoBeer);
+      });
+  });
 
-  useEffect(() => {
-    fetch('https://thecocktaildb.com/api/json/v1/1/random.php')
-      .then((response) => response.json())
-      .then((randoCocktail) => {
-        let cocktailName = randoCocktail.drinks[0].strDrink
-        let cocktailPic = randoCocktail.drinks[0].strDrinkThumb
+  useEffect(() => displayCocktail, []);
 
-        setDrinkName(cocktailName)
-        setDrinkPic(cocktailPic)
-      })
-  }, [])
   function displayCocktail() {
-    fetch('https://thecocktaildb.com/api/json/v1/1/random.php')
+    fetch("https://thecocktaildb.com/api/json/v1/1/random.php")
       .then((response) => response.json())
       .then((randoCocktail) => {
-        let cocktailName = randoCocktail.drinks[0].strDrink
-        let cocktailPic = randoCocktail.drinks[0].strDrinkThumb
+        let cocktailName = randoCocktail.drinks[0].strDrink;
+        let cocktailPic = randoCocktail.drinks[0].strDrinkThumb;
 
-        setDrinkName(cocktailName)
-        setDrinkPic(cocktailPic)
-      })
+        setDrinkName(cocktailName);
+        setDrinkPic(cocktailPic);
+      });
   }
 
   return (
@@ -45,6 +36,6 @@ function CocktailCorner() {
         <img id="cocktail-pic" src={drinkPic} alt="A Lovely Libation"></img>
       </div>
     </div>
-  )
+  );
 }
-export default CocktailCorner
+export default CocktailCorner;
