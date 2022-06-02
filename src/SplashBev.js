1f6f78274ddac1b5d4dceb18e8e37b76a38d6bd9
brian corker <bcorker123@gmail.com>
2022-06-02 11:32:45 -0400

    added tastingnotes form functionality & listingcard component & randombev functionality

--- src/SplashBev.js
+++ src/SplashBev.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import MainContainer from './MainContainer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import MainContainer from "./MainContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function SplashBev() {
-    return <>
-    <div className="welcome-card">
+  return (
+    <>
+      <br />
+      <div className="welcome-card">
         <div>
-        <img src={require("./testing-img/bb-icon.png")} alt="placeholder" className="bbimg" />
-            <h1>Welcome to beerbites!</h1> <p>Want to have a new tasty beer with your lunch or dinner? Looking for a spot to check out to finally enjoy that Pilsner you've been thinking about? Well, you are in the right place. Take a second to look around and plan out all of the delicous drinks that lay ahead.</p>
+          <img
+            src={require("./testing-img/bb-icon.png")}
+            alt="placeholder"
+            className="bbimg"
+          />
+          <h1>Welcome to beerbites!</h1>{" "}
+          <p>
+            Want to have a new tasty beer with your lunch or dinner? Looking for
+            a spot to check out to finally enjoy that Pilsner you've been
+            thinking about? Well, you are in the right place. Take a second to
+            look around and plan out all of the delicous drinks that lay ahead.
+          </p>
         </div>
         <MainContainer />
-    </div>
-    
+      </div>
     </>
+  );
 }
 
 export default SplashBev;
